735ca6ee59268558d90ad4b75337bfa9ed5acee1
VenkatKunaparaju <venkatkrox@gmail.com>
2023-01-13 18:18:43 -0500

    Finished Prelim socket connections

--- main.cc
+++ main.cc
@@ -15,42 +15,66 @@
 #define MAXQ 5
 int main() {
     
+
     //Set address
     struct sockaddr_in address; 
-    memset( &address, 0, sizeof(address) );
+    int aSize = sizeof(address);
+    memset( &address, 0, aSize );
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons((u_short) PORT);
 
+
+    int error;
     //Define master socket
-    int masterSocket;
-    if ((masterSocket = socket(AF_INET, SOCK_STREAM, 0)) <= 0) {
+    int masterSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (masterSocket <= 0) {
         perror("Master socket error");
         exit( -1 );
     }
     
     //Add Socket options
     int option = 1;
-    if (setsockopt(masterSocket, SOL_SOCKET, SO_REUSEADDR, (char *) &option, sizeof(option))) {
+    error = setsockopt(masterSocket, SOL_SOCKET, SO_REUSEADDR, (char *) &option, sizeof(option));
+    if (error) {
         perror("Socket options error");
         exit(-1);
     }
-    
+
+    //Bind socket to address
+    error = bind( masterSocket, (struct sockaddr *)&address, aSize );
+    if ( error ) {
+        perror("Binding error");
+        exit( -1 );
+    }
     
     
     //Set socket in listen mode to read user requests
-    if (listen(masterSocket, MAXQ) <= 0) {
+    error = listen(masterSocket, MAXQ);
+    if (error) {
         perror("Listening error");
         exit(-1);
     }
+
+    //Continuously serve clients
+    while(1) {
+        // Accept incoming client connections
+        int slaveSocket = accept( masterSocket,
+                    (struct sockaddr *)&address,
+                    (socklen_t*)&aSize);
+
+        if ( slaveSocket < 0 ) {
+            perror( "accept" );
+            exit( -1 );
+        }
+
+        //Test writing to client
+        const char * prompt = "Hello";
+        write(slaveSocket, prompt, strlen(prompt));
+
+        //Close client connection
+        close(slaveSocket);
+    }
    
-
-    
-
-
-
-
-
-    
 
 }
